93c789de31b44c41699a439c6d74ebbfb480f5b2
PSNITS <136683734+PSNITS@users.noreply.github.com>
2023-10-19 18:18:50 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,43 +1,44 @@
 const express = require("express");
-const cors = require("cors");
 const bodyParser = require("body-parser");
-const stripe = require("stripe")("sk_test_51Nen7hFpqCcVujOth5gJ7Y9SSImVsnFzXOuVxADd3cwX2ILLRz3lMADgsKRbCwkM57egqdQGeTThkxHA2TGEz18S00XdyXfiTa");
+const cors = require("cors")
+const app = express();
+// This is your test secret API key.
 
-const app = express()
-app.use(express.json());
 app.use(cors());
-app.use(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.urlencoded({extended:true}))
 
+const stripe = require("stripe")('sk_test_51Nen7hFpqCcVujOth5gJ7Y9SSImVsnFzXOuVxADd3cwX2ILLRz3lMADgsKRbCwkM57egqdQGeTThkxHA2TGEz18S00XdyXfiTa');
 
-app.post("/api/create-checkout-session",async(req,res)=>{
-    const {products} = req.body;
-    const arr = [0]
-    
-    const lineItems = arr.map((product)=>({
-        price_data:{
-            currency:"USD",
-            product_data:{
-                name:products.name,
-            },
-            unit_amount:products.amount * 100,
-        },
-        quantity:1,
-    }));    
-    
-    const session = await stripe.checkout.sessions.create({
-        payment_method_types:["card"],
-        line_items:lineItems,
-        mode:"payment",
-        success_url:"https://test-v.mayleefun.xxx/sucess",
-        cancel_url:"https://test-v.mayleefun.xxx/cancel"
-    })
+app.use(express.static("public"));
+app.use(express.json());
 
-    res.json({id:session.id})
+const calculateOrderAmount = (items) => {
+  // Replace this constant with a calculation of the order's amount
+  // Calculate the order total on the server to prevent
+  // people from directly manipulating the amount on the client
+  return items * 100;
+};
 
-})
+app.post("/create-payment-intent", async (req, res) => {
+  const { items } = req.body;
+  const prc = items.price
+  const name = items.name;
+  // Create a PaymentIntent with the order amount and currency
+  const paymentIntent = await stripe.paymentIntents.create({
+    amount: calculateOrderAmount(prc),
+    currency: "inr",
+    description:`${name}`,
+    // In the latest version of the API, specifying the `automatic_payment_methods` parameter is optional because Stripe enables its functionality by default.
+    automatic_payment_methods: {
+      enabled: true,
+    },
+  });
+  console.log(paymentIntent);
 
-
-
-app.listen(4000,()=>{
-    console.log("server is running");
+  res.send({
+    clientSecret: paymentIntent.client_secret,
+  });
 });
+
+
+app.listen(4242, () => console.log("Node server listening on port 4242!"));
